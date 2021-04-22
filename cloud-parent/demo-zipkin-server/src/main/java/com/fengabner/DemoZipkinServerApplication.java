de7fed60848d02f62a18fc7f7d8c3cfd316bc9cb
zhongmx <42760465+zhongmx@users.noreply.github.com>
2021-04-22 17:34:00 +0800

    zipkin

--- cloud-parent/demo-zipkin-server/src/main/java/com/fengabner/DemoZipkinServerApplication.java
+++ cloud-parent/demo-zipkin-server/src/main/java/com/fengabner/DemoZipkinServerApplication.java
@@ -2,7 +2,12 @@ package com.fengabner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 import zipkin2.server.internal.EnableZipkinServer;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableZipkinServer // 开启Zipkin 服务器功能
@@ -10,6 +15,11 @@ public class DemoZipkinServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoZipkinServerApplication.class, args);
+    }
+
+    @Bean
+    public PlatformTransactionManager txManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
     }
 
 }
